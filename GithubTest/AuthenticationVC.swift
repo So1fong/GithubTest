ceaf549247de61e38e7cbafd8c9a27b40924480a
Ekaterina <zireael6@gmail.com>
2019-03-15 11:43:20 +0300

    добавлена страница авторизации

---
+++ GithubTest/AuthenticationVC.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by Ekaterina Kozlova on 15.03.2019.
+//  Copyright © 2019 Ekaterina Kozlova. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationVC: UIViewController
+{
+    @IBOutlet weak var gitHubImageView: UIImageView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var authButton: UIButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        let imageName = "GitHub-Mark.png"
+        let image = UIImage(named: imageName)
+        gitHubImageView.image = image
+    }
+    
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+    
+    @IBAction func authButtonTapped(_ sender: Any)
+    {
+        
+    }
+    
+}
+
